2b060f31f41ebc64f10ccc532f39b00e5462c4a2
879439 <879439@stud.unive.it>
2021-12-13 20:12:40 +0100

    Comunicazione tra app e server

---
+++ app/src/main/java/com/example/ssh/LoginResult.java
@@ -0,0 +1,17 @@
+package com.example.ssh;
+
+
+
+public class LoginResult {
+    private String nome;
+
+    private String cognome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+}
--- app/src/main/java/com/example/ssh/MainActivity.java
+++ app/src/main/java/com/example/ssh/MainActivity.java
@@ -11,20 +11,69 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://10.0.2.2:3000";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
         if (checkPermission()) {
             // if permission is already granted display a toast message
             Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
         } else
             requestPermission();
+        Button loginBtn = findViewById(R.id.Login);
+        EditText nome = findViewById(R.id.editTextTextEmailAddress);
+        EditText cognome = findViewById(R.id.editTextTextPassword);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("nome",nome.getText().toString());
+                map.put("cognome",cognome.getText().toString());
+                Call<LoginResult> call = retrofitInterface.executeLogin(map);
+                call.enqueue(new Callback<LoginResult>() {
+                    @Override
+                    public void onResponse(Call<LoginResult> call, Response<LoginResult> response) {
+                        if(response.code()==200){
+                            Toast.makeText(MainActivity.this, "Login effettuato", Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(MainActivity.this, "Credenziali errate", Toast.LENGTH_LONG).show();
+                        }
 
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResult> call, Throwable t) {
+                        System.out.println(t.getMessage());
+                        Toast.makeText(MainActivity.this,  t.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
     }
 
     public boolean checkPermission() {
@@ -53,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
     public void next(View v){
         startActivity(new Intent(this, Schermata_Principale.class));
     }
